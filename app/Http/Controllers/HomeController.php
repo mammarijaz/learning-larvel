b314d8cd504fc2c8aabbaddc47f84e1675f37848
Ammar Ijaz <ammarijaz@outlook.com>
2018-09-07 01:52:50 +0500

    Code committed lecture 11

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        return view( 'posts.home' )->with( 'posts' , $user->posts );
     }
 }
